bcf7e3e5f5fc83d9fd73387c8a51b969f51dd8ed
ngodi <ngodi@DESKTOP-VEGTPUL>
2025-10-25 17:43:11 +0700

    Add project files.

---
+++ WebBanHang/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Iphone()
+        {
+            return View();
+        }
+        public ActionResult Ipad()
+        {
+            return View();
+        }
+        public ActionResult Mac()
+        {
+            return View();
+        }
+        public ActionResult Watch()
+        {
+            return View();
+        }
+        public ActionResult Audio()
+        {
+            return View();
+        }
+        public ActionResult Accessory()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBanHang/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHang.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Storage { get; set; }
+        public decimal Price { get; set; }
+    }
+}
