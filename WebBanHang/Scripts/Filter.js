3871f180c7c75c039c0770426f7a67f7951af158
thienngo2910 <ngodinhphucthien@gmail.com>
2025-10-26 00:59:04 +0700

    làm menu ẩn hiện

---
+++ WebBanHang/Scripts/Filter.js
@@ -0,0 +1,12 @@
+﻿const iphoneSeriesList = ['iphone17', 'iphone16', 'iphone15', 'iphone14', 'iphone13'];
+
+iphoneSeriesList.forEach(series => {
+    const mainCheckbox = document.getElementById(`${series}Main`);
+    const subMenu = document.getElementById(`${series}Series`);
+
+    if (mainCheckbox && subMenu) {
+        mainCheckbox.addEventListener('change', function () {
+            subMenu.style.display = this.checked ? 'block' : 'none';
+        });
+    }
+});
